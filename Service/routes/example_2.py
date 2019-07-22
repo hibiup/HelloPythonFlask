4c5f5e24e113914c55e1bce3cce7f96aef61466b
326487162 <Welcome@661>
2019-07-22 16:36:39 -0400

    Add flask-restplus example

--- Service/routes/example_2.py
+++ Service/routes/example_2.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 
 """
 与原生 Flask 不同，Flask rest plus 引入了一个 Api, 并通过这个 Api 来配置应用接口，提供额外服务。
@@ -22,17 +22,24 @@ api = Api(flask)
 
   * URL 可以带参数。
   * 服务必须是基于 Resource 的类。类中的成员函数名称表明访问的方法。
+  * 集成的 Swagger 装饰器可以生成 API 文档。允许修饰在 class 或方法层。
 """
 @api.route('/hello/<string:name>')
+@api.doc(params={'name': 'User name'})
 class HelloWorld(Resource):
     """
       函数名代表访问方法，可以接受来自 URL 的参数，URL 参数支持路径(/value1/value2/...)参数和请求参数(?key1=value1&key2=value2...)：
         $ curl -X GET http://localhost:5000/hello/xxx?key=value
 
     """
+    @api.doc(responses={403: 'Not Authorized'})
     def get(self, name):
         print(request.headers['content-type'])
         print(request.args['key'])
+        """
+         与原生 Flask 中实现 JSONResponse 不同，RESTPlus 提供了一个 marshal_with 装饰器来实现对输入和输出数据类型的转换，
+         本例不打算演示，具体参考 example_3 和：https://flask-restplus.readthedocs.io/en/stable/marshalling.html
+        """
         return {'hello': '{0}'.format(name)}  # 返回 JSON
 
     """
