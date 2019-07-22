3c21a9dbc988cc254a7d51343021edcf86723b94
326487162 <Welcome@661>
2019-07-22 16:51:53 -0400

    Add flask-restplus example

--- Service/routes/example_2.py
+++ Service/routes/example_2.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restplus import Resource, Api, fields
 
 """
+https://flask-restplus.readthedocs.io/en/stable/index.html
+
 与原生 Flask 不同，Flask rest plus 引入了一个 Api, 并通过这个 Api 来配置应用接口，提供额外服务。
 """
 
@@ -22,15 +24,15 @@ api = Api(flask)
 
   * URL 可以带参数。
   * 服务必须是基于 Resource 的类。类中的成员函数名称表明访问的方法。
-  * 集成的 Swagger 装饰器可以生成 API 文档。允许修饰在 class 或方法层。
+  * 集成的 Swagger 装饰器可以生成 API 文档。允许修饰在 class 或方法层。(https://flask-restplus.readthedocs.io/en/stable/swagger.html)
 """
 @api.route('/hello/<string:name>')
 @api.doc(params={'name': 'User name'})
 class HelloWorld(Resource):
+    parser = api.parser()
     """
       函数名代表访问方法，可以接受来自 URL 的参数，URL 参数支持路径(/value1/value2/...)参数和请求参数(?key1=value1&key2=value2...)：
         $ curl -X GET http://localhost:5000/hello/xxx?key=value
-
     """
     @api.doc(responses={403: 'Not Authorized'})
     def get(self, name):
